8c87e79a291fa3553cd4dcb4831a22b3873d6404
CodeSevenic <sbu.audio.production@gmail.com>
2021-12-25 17:52:40 +0200

    code update

--- src/assets/js/components/store-slide.js
+++ src/assets/js/components/store-slide.js
@@ -51,3 +51,35 @@ export const storeCatalogs = new Swiper('.store-catalogs', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+export const mainCatalogs = new Swiper('.main-catalogs', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      // spaceBetween: 10,
+    },
+    575: {
+      slidesPerView: 2,
+      // spaceBetween: 20,
+    },
+    820: {
+      slidesPerView: 3,
+      // spaceBetween: 40,
+    },
+    1200: {
+      slidesPerView: 4,
+      // spaceBetween: 50,
+    },
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
--- template-parts/content-catalog_body.php
+++ template-parts/content-catalog_body.php
@@ -9,50 +9,56 @@ $args = [
 $post_query = new WP_Query($args);
 ?>
 <section class="section-container">
-  <ul class="list flex-division">
-    <?php
-    while ($post_query->have_posts()) {
-      $post_query->the_post();
-      if (get_field('poster_image', get_the_ID())) {
-        $image = get_field('poster_image', get_the_ID())['sizes']['catalog-image'];
-      }
 
-    ?>
-      <li class="list-item li-brochu">
-        <article class="catalog ga-brochure-catalog">
-          <div class="catalog-cover">
-            <a class="catalog-anchor" href="<?php the_permalink(); ?>">
-              <div class="catalog-image">
-                <div class="catalog-border">
-                  <img src="<?php if ($image) echo $image ?>" alt="Catalogue Game Johannesburg">
-                  <div class="catalog-mask"> <span class="button button-bold button-primary">View Catalog</span> </div>
+  <div class="main-catalogs">
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
+    <ul class="swiper-wrapper">
+      <?php
+      while ($post_query->have_posts()) {
+        $post_query->the_post();
+        if (get_field('poster_image', get_the_ID())) {
+          $image = get_field('poster_image', get_the_ID())['sizes']['catalog-image'];
+        }
+
+      ?>
+        <li class="li-brochu swiper-slide">
+          <article class="catalog ga-brochure-catalog">
+            <div class="catalog-cover">
+              <a class="catalog-anchor" href="<?php the_permalink(); ?>">
+                <div class="catalog-image">
+                  <div class="catalog-border">
+                    <img src="<?php if ($image) echo $image ?>" alt="Catalogue Game Johannesburg">
+                    <div class="catalog-mask"> <span class="button button-bold button-primary">View Catalog</span> </div>
+                  </div>
                 </div>
-              </div>
-            </a>
-          </div>
-          <header class="catalog-header">
-            <span class="catalog-date">
-              <?php echo get_field('start_date') ?> - <?php echo get_field('end_date') ?> </span>
-            <?php if (get_field('cover_title')) { ?>
-              <h4 class="catalog-title"><?php echo get_field('cover_title') ?></h4>
-            <?php } ?>
-          </header>
-          <?php
-          if (get_field('store_logo')) { ?>
-            <div class="catalog-logo"> <a href="<?php the_permalink() ?>" class="city_slider-logo-link ">
-                <img src="<?php echo get_field('store_logo')['sizes']['catalog-logo'] ?>" alt="">
-              </a> </div> <?php } else { ?>
-            <div class="catalog-website">
-              <a href="<?php the_permalink() ?>" target="_blank"><?php the_title() ?></a>
+              </a>
             </div>
-          <?php }
-          ?>
-        </article>
-      </li>
-    <?php   }
-    wp_reset_postdata();
-    ?>
-  </ul>
+            <header class="catalog-header">
+              <span class="catalog-date">
+                <?php echo get_field('start_date') ?> - <?php echo get_field('end_date') ?> </span>
+              <?php if (get_field('cover_title')) { ?>
+                <h4 class="catalog-title"><?php echo get_field('cover_title') ?></h4>
+              <?php } ?>
+            </header>
+            <?php
+            if (get_field('store_logo')) { ?>
+              <div class="catalog-logo"> <a href="<?php the_permalink() ?>" class="city_slider-logo-link ">
+                  <img src="<?php echo get_field('store_logo')['sizes']['catalog-logo'] ?>" alt="">
+                </a> </div> <?php } else { ?>
+              <div class="catalog-website">
+                <a href="<?php the_permalink() ?>" target="_blank"><?php the_title() ?></a>
+              </div>
+            <?php }
+            ?>
+          </article>
+        </li>
+      <?php   }
+      wp_reset_postdata();
+      ?>
+    </ul>
+    <div class="swiper-pagination main-pagination"></div>
+  </div>
   <div class="content-block view-all-brochures" align="center"> <button onclick="gaStats.trackStatsEvents(this);" data-content=".column-main-five-rows .list-for-5-no-left-banner" data-page="1" data-max="15" data-url="johannesburg" class="button button-primary btn_flex-wrap__button pagination-btn ga-btn-brochures-see-more">See more</button> </div>
 </section>
 <!-- </section> -->
