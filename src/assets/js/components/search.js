5daef94c0cd35a58aea0c8780b035e15c32d5533
CodeSevenic <sbu.audio.production@gmail.com>
2022-01-05 21:21:35 +0200

    code update

--- src/assets/js/components/search.js
+++ src/assets/js/components/search.js
@@ -20,7 +20,7 @@ class Search {
 
   // 2. events
   events() {
-    this.icon.addEventListener('click', this.iconClick.bind(this));
+    // this.icon.addEventListener('click', this.iconClick.bind(this));
     this.search_input.addEventListener('input', this.onInput.bind(this));
     this.clear.addEventListener('click', this.onClear.bind(this));
   }
