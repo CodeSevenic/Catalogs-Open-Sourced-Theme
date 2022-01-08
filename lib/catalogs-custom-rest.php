a7ee5a487b3422d4a7ee644b2540f157ad26b7b2
CodeSevenic <sbu.audio.production@gmail.com>
2022-01-08 21:09:06 +0200

    code update

--- lib/catalogs-custom-rest.php
+++ lib/catalogs-custom-rest.php
@@ -18,9 +18,52 @@ function catalogsRegisterSearch()
   ));
 }
 
-function catalogsSearchResults()
+function catalogsSearchResults($data)
 {
-  return 'Congrats you created a route';
+  $storeQuery = new WP_Query(array(
+    'post_type' => array('store'),
+    's' => sanitize_text_field($data['term']),
+  ));
+  $results = array(
+    'stores' => array(),
+    'categories' => array(),
+    'catalogs' => array()
+  );
+
+  while ($storeQuery->have_posts()) {
+    $storeQuery->the_post();
+    array_push($results['stores'], array(
+      'title' => get_the_title(),
+      'link' => get_the_permalink(),
+      'coverTitle' => get_field('cover_title'),
+    ));
+
+    $catalog_content = get_field('catalog_content');
+    foreach ($catalog_content as $content) {
+      array_push($results['catalogs'], $content['catalog_title']);
+    }
+  }
+  wp_reset_postdata();
+
+  $args = array(
+    'taxonomy'      => array('store_type'),
+    'orderby'       => 'id',
+    'order'         => 'ASC',
+    'hide_empty'    => false,
+    'fields'        => 'all',
+    'name__like'    => $data['term']
+  );
+
+
+
+  $terms = get_terms($args);
+
+  foreach ($terms as $tax) {
+    array_push($results['categories'], array('category_name' => $tax->name, 'category_link' => get_term_link($tax->term_id, 'store_type')));
+  }
+
+
+  return $results;
 }
 
 add_action('rest_api_init', 'catalogsRegisterSearch');
