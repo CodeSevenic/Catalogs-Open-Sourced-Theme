c342e7c54d15c353e57f427704b122aaf9298970
CodeSevenic <sbu.audio.production@gmail.com>
2022-01-15 21:53:41 +0200

    code update

--- single-store.php
+++ single-store.php
@@ -9,7 +9,8 @@ $catalog_content = get_field('catalog_content', get_the_ID());
 
 <section class="section-container container mx-auto">
   <!-- banner image -->
-  <?php if ($banners) { ?><div class="store_banner">
+  <?php if ($banners) { ?>
+    <div class="store_banner">
       <div class="swiper-wrapper">
         <?php
         if ($banners) : ?>
@@ -23,47 +24,52 @@ $catalog_content = get_field('catalog_content', get_the_ID());
       </div>
     </div>
   <?php } ?>
+
   <h1 class="font-semibold text-xl lg:text-2xl my-4 px-[5%] text-center md:text-left">View all featured <span class="text-sky-700">specials</span>, <span class="text-sky-600">deals</span>, <span class="text-sky-500">magazines</span> and <span class="text-sky-800">catalogues</span> from <img class="inline w-[100px]" src="<?php echo get_field('store_logo')['sizes']['catalog-logo'] ?>" alt="<?php the_title() ?>">
   </h1>
 
   <hr class="py-3  border-sky-500 mx-auto w-[80%] md:w-[95%]">
-  <div class="store-wrapper">
+
+  <div class="catalogs-frame relative">
     <div class="swiper-button-prev"></div>
     <div class="swiper-button-next"></div>
-    <div class="store-catalogs">
+    <div class="main-catalogs">
+      <!-- <div class="store-catalogs"> -->
       <ul class="swiper-wrapper" style="display: flex; list-style: none; margin: 0;">
         <?php
         if ($catalog_content) { ?>
           <?php foreach ($catalog_content as $catalog_image) {
           ?>
-            <li class="li-brochu swiper-slide card-size">
-              <article class="catalog ga-brochure-catalog">
-                <div class="catalog-cover">
-                  <a class="catalog-anchor" href="https://allcatalogues.co.za/johannesburg/game">
-                    <div class="catalog-image">
-                      <div class="catalog-border">
-                        <img src="<?php echo $catalog_image['catalog_image']['url']; ?>" alt="">
+            <li class="li-brochu swiper-slide">
+              <article class="cover">
+                <div class="catalog ga-brochure-catalog">
+                  <div class="catalog-cover">
+                    <a class="catalog-cover" href="https://allcatalogues.co.za/johannesburg/game">
+                      <div class="catalog-image">
+                        <div class="catalog-border">
+                          <img src="<?php echo $catalog_image['catalog_image']['url']; ?>" alt="">
 
-                        <div class="catalog-mask"> <span class="button button-bold button-primary">View Catalog</span> </div>
+                          <!-- <div class="catalog-mask"> <span class="button button-bold button-primary">View Catalog</span> </div> -->
+                        </div>
                       </div>
-                    </div>
-                  </a>
-                </div>
-                <header class="catalog-header">
-                  <span class="catalog-date">
-                    <?php echo get_field('start_date') ?> - <?php echo get_field('end_date') ?> </span>
-                  <h4 class="catalog-title"><?php echo $catalog_image['catalog_title'] ?></h4>
-                </header>
-                <?php
-                if (get_field('store_logo')) { ?>
-                  <div class="catalog-logo"> <a href="<?php echo get_post_type_archive_link('store') ?>" class="city_slider-logo-link ">
-                      <img src="<?php echo get_field('store_logo')['sizes']['catalog-logo'] ?>" alt="">
-                    </a> </div> <?php } else { ?>
-                  <div class="catalog-website">
-                    <a href="" target="_blank"><?php the_title() ?></a>
+                    </a>
                   </div>
-                <?php }
-                ?>
+                  <header class="catalog-header">
+                    <span class="catalog-date">
+                      <?php echo get_field('start_date') ?> - <?php echo get_field('end_date') ?> </span>
+                    <h4 class="catalog-title"><?php echo $catalog_image['catalog_title'] ?></h4>
+                  </header>
+                  <?php
+                  if (get_field('store_logo')) { ?>
+                    <div class="catalog-logo"> <a href="<?php echo get_post_type_archive_link('store') ?>" class="city_slider-logo-link ">
+                        <img src="<?php echo get_field('store_logo')['sizes']['catalog-logo'] ?>" alt="">
+                      </a> </div> <?php } else { ?>
+                    <div class="catalog-website">
+                      <a href="" target="_blank"><?php the_title() ?></a>
+                    </div>
+                  <?php }
+                  ?>
+                </div>
               </article>
             </li>
         <?php
@@ -73,10 +79,10 @@ $catalog_content = get_field('catalog_content', get_the_ID());
         ?>
       </ul>
       <div class="swiper-pagination pagination-pos"></div>
+      <!-- </div> -->
     </div>
   </div>
 
-  </div>
 
 
 </section>
